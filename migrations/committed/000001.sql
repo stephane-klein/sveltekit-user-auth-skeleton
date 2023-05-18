535652ceffae51d5711e414231f436d58d028cd9
Stéphane Klein <contact@stephane-klein.info>
2023-05-18 17:19:16 +0200

    Append many fields to users table

--- migrations/committed/000001.sql
+++ migrations/committed/000001.sql
@@ -2,8 +2,20 @@
 --! Hash: sha1:0b9d64dceaac782151a84fa40ab92bf759457c58
 
 -- Enter migration here
+CREATE EXTENSION IF NOT EXISTS citext;
+
 DROP TABLE IF EXISTS public.users CASCADE;
 CREATE TABLE public.users (
     id                     SERIAL PRIMARY KEY,
-    username               VARCHAR NULL UNIQUE
+    username               CITEXT NULL UNIQUE,
+    first_name             VARCHAR(150) DEFAULT NULL,
+    last_name              VARCHAR(150) DEFAULT NULL,
+    email                  CITEXT DEFAULT NULL,
+    password               VARCHAR(255) DEFAULT NULL,
+    is_staff               BOOLEAN DEFAULT false,
+    is_active              BOOLEAN DEFAULT false,
+    last_login             TIMESTAMP WITH TIME ZONE DEFAULT NULL,
+    date_joined            TIMESTAMP WITH TIME ZONE DEFAULT NULL,
+    created_at             TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at             TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
--- sqls/fixtures.sql
+++ sqls/fixtures.sql
@@ -6,17 +6,37 @@ TRUNCATE public.users;
 
 INSERT INTO public.users
     (
-        username
+        username,
+        first_name,
+        last_name,
+        email,
+        is_staff,
+        is_active
     )
     VALUES
     (
-        'user1'
+        'user1',                 -- username
+        'John1',                 -- first_name
+        'Doe1',                  -- last_name
+        'john.doe1@example.com',  -- email
+        true,                    -- is_staff
+        true                     -- is_active
     ),
     (
-        'user2'
+        'user2',                 -- username
+        'John2',                 -- first_name
+        'Doe2',                  -- last_name
+        'john.doe2@example.com',  -- email
+        false,                    -- is_staff
+        true                     -- is_active
     ),
     (
-        'user3'
+        'user3',                 -- username
+        'John3',                 -- first_name
+        'Doe3',                  -- last_name
+        'john.doe3@example.com',  -- email
+        false,                    -- is_staff
+        true                     -- is_active
     );
 
 \echo "Fixtures loaded"

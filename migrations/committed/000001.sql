f7bd5e5dd2ef84d7fed613eb151ee743763372cd
Stéphane Klein <contact@stephane-klein.info>
2023-05-18 18:26:12 +0200

    Add create_user SQL function

--- migrations/committed/000001.sql
+++ migrations/committed/000001.sql
@@ -2,15 +2,15 @@
 --! Hash: sha1:0b9d64dceaac782151a84fa40ab92bf759457c58
 
 -- Enter migration here
-CREATE EXTENSION IF NOT EXISTS citext;
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 DROP TABLE IF EXISTS public.users CASCADE;
 CREATE TABLE public.users (
     id                     SERIAL PRIMARY KEY,
-    username               CITEXT NULL UNIQUE,
+    username               VARCHAR(100) NULL UNIQUE,
     first_name             VARCHAR(150) DEFAULT NULL,
     last_name              VARCHAR(150) DEFAULT NULL,
-    email                  CITEXT DEFAULT NULL,
+    email                  VARCHAR(360) DEFAULT NULL,
     password               VARCHAR(255) DEFAULT NULL,
     is_staff               BOOLEAN DEFAULT false,
     is_active              BOOLEAN DEFAULT false,
@@ -19,3 +19,36 @@ CREATE TABLE public.users (
     created_at             TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
     updated_at             TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
+
+CREATE OR REPLACE FUNCTION create_user(
+    username               VARCHAR(100),
+    first_name             VARCHAR(150),
+    last_name              VARCHAR(150),
+    email                  VARCHAR(360),
+    password               VARCHAR(255),
+    is_staff               BOOLEAN,
+    is_active              BOOLEAN
+) RETURNS INTEGER
+LANGUAGE sql
+AS $$
+    INSERT INTO public.users
+    (
+        username,
+        first_name,
+        last_name,
+        email,
+        password,
+        is_staff,
+        is_active
+    )
+    VALUES(
+        TRIM(username),
+        TRIM(first_name),
+        TRIM(last_name),
+        LOWER(TRIM(email)),
+        CRYPT(TRIM(password), gen_salt('bf', 8)),
+        is_staff,
+        is_active
+    )
+    RETURNING id;
+$$;
--- sqls/fixtures.sql
+++ sqls/fixtures.sql
@@ -4,39 +4,32 @@ SET client_min_messages TO WARNING;
 
 TRUNCATE public.users;
 
-INSERT INTO public.users
-    (
-        username,
-        first_name,
-        last_name,
-        email,
-        is_staff,
-        is_active
-    )
-    VALUES
-    (
-        'user1',                 -- username
-        'John1',                 -- first_name
-        'Doe1',                  -- last_name
-        'john.doe1@example.com',  -- email
-        true,                    -- is_staff
-        true                     -- is_active
-    ),
-    (
-        'user2',                 -- username
-        'John2',                 -- first_name
-        'Doe2',                  -- last_name
-        'john.doe2@example.com',  -- email
-        false,                    -- is_staff
-        true                     -- is_active
-    ),
-    (
-        'user3',                 -- username
-        'John3',                 -- first_name
-        'Doe3',                  -- last_name
-        'john.doe3@example.com',  -- email
-        false,                    -- is_staff
-        true                     -- is_active
-    );
+SELECT create_user(
+    username   => 'john-doe1',
+    first_name => 'John',
+    last_name  => 'Doe1',
+    email      => 'john.doe1@example.com',
+    password   => 'secret1',
+    is_staff   => true,
+    is_active  => true
+);
 
-\echo "Fixtures loaded"
+SELECT create_user(
+    username   => 'john-doe2',
+    first_name => 'John',
+    last_name  => 'Doe2',
+    email      => 'john.doe2@example.com',
+    password   => 'secret2',
+    is_staff   => false,
+    is_active  => true
+);
+
+SELECT create_user(
+    username   => 'john-doe3',
+    first_name => 'John',
+    last_name  => 'Doe3',
+    email      => 'john.doe3@example.com',
+    password   => 'secret3',
+    is_staff   => false,
+    is_active  => false
+);

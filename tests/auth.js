74cce34f1b754d42dc95f101144dc3f18815d82a
Stéphane Klein <contact@stephane-klein.info>
2023-08-27 21:15:33 +0200

    Implement auth.session database unit tests #6

--- tests/auth.js
+++ tests/auth.js
@@ -2,7 +2,6 @@ import postgres from "postgres";
 
 let sql;
 
-
 beforeAll(() => {
     sql = postgres(
         'postgres://postgrestest:passwordtest@localhost:5433/myapp'
@@ -12,20 +11,55 @@ afterAll(() => {
     sql.end()
 });
 
-test('Create a user', async () => {
+test("Create a user and login", async() => {
     await sql`DELETE FROM auth.users`;
+    await sql`DELETE FROM auth.sessions`;
 
-    await sql`SELECT auth.create_user(
+    const userId = (await sql`SELECT auth.create_user(
         username   => 'john-doe',
         first_name => 'John',
         last_name  => 'Doe',
-        email      => 'john.doe@gmail.com',
+        email      => 'john.doe@example.com',
         password   => 'secret',
         is_staff   => false,
         is_active  => true
-    )`;
+    )`)[0]?.create_user;
 
     expect(
         (await sql`SELECT COUNT(*)::INTEGER FROM auth.users WHERE first_name = 'John'`)[0].count
     ).toBe(1);
+
+    let authenticateResult = (await sql`
+        SELECT auth.authenticate(
+            input_username =>'',
+            input_email    =>'john.doe@example.com',
+            input_password =>'secret'
+        )
+    `)[0]?.authenticate;
+
+    expect(authenticateResult.status_code).toBe(200);
+
+    expect(
+        (await sql`SELECT user_id FROM auth.sessions WHERE id=${authenticateResult.session_id}`)[0].user_id
+    ).toBe(userId);
+
+    authenticateResult = (await sql`
+        SELECT auth.authenticate(
+            input_username =>'',
+            input_email    =>'john.doe@example.com',
+            input_password =>'badpassword'
+        )
+    `)[0]?.authenticate;
+
+    expect(authenticateResult.status_code).toBe(401);
+
+    authenticateResult = (await sql`
+        SELECT auth.authenticate(
+            input_username =>'',
+            input_email    =>'unknow@example.com',
+            input_password =>'secret'
+        )
+    `)[0]?.authenticate;
+
+    expect(authenticateResult.status_code).toBe(401);
 });

b4f09e4d95c97264e210b687fe6818773aea73ca
Stéphane Klein <contact@stephane-klein.info>
2023-08-27 19:25:34 +0200

    Improve login page

--- src/routes/(public)/login/+page.svelte
+++ src/routes/(public)/login/+page.svelte
@@ -4,14 +4,14 @@
 
 <form method="POST">
     {#if form?.error}<p class="error">{form.error}</p>{/if}
-    <label>
-        Email
-        <input name="email" type="email" value={form?.email ?? ""} />
-    </label>
-    <label>
-        Password
-        <input name="password" type="password" />
-    </label>
+    <div>
+        <label for="email">Email:</label>
+        <input id="email" name="email" type="email" value={form?.email ?? ""} />
+    </div>
+    <div>
+        <label for="password">Password:</label>
+        <input id="password" name="password" type="password" />
+    </div>
 
     <input type="submit" value="Login" />
 </form>

08c0d1d4bd6d133e5a17dcfa38d3647f11bf0528
Stéphane Klein <contact@stephane-klein.info>
2023-09-03 20:52:05 +0200

    Fix bug in ./scripts/init.sh, I had forget to run the migration

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,3 +6,5 @@ cd "$(dirname "$0")/../"
 docker compose up -d postgres postgres-test --wait
 pnpm run migrate:reset --erase
 pnpm run migrate-test:reset --erase
+pnpm run migrate:watch --once
+pnpm run migrate-test:watch --once

ccd88006bfe60ba7c553e61fa3cdaa80d8868a58
Stéphane Klein <contact@stephane-klein.info>
2023-09-03 23:07:51 +0200

    Fix mistake

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,7 +3,7 @@ set -e
 
 cd "$(dirname "$0")/../"
 
-docker compose up -d postgres postgres-test --wait
+docker compose up -d --wait
 pnpm run migrate:reset --erase
 pnpm run migrate-test:reset --erase
 pnpm run migrate:watch --once
